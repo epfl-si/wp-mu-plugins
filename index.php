e6fcf5cc6a1f7cbf87d8e6e48ee6098f04701b88
SaphireVert <jerome.cosandey@gmail.com>
2023-03-15 11:08:37 +0100

    feat: get last modified posts
    includes name and limit parameters

--- index.php
+++ index.php
@@ -18,6 +18,20 @@ function getLastChange( $data ){
     return $results;
 }
 
+function getLastRevisions( $data ){
+    global $wpdb;
+    $name = $data->get_param( 'name' );
+    $name = $name ? $name : '';
+    $limit = $data->get_param( 'limit' );
+    $limit = $limit ? $limit : 5;
+    $sql = $wpdb->prepare( "SELECT wp_posts.post_title, wp_users.user_login AS username, post_modified AS last_modified FROM `wp_posts` 
+                            LEFT JOIN wp_users ON wp_users.ID = wp_posts.post_author 
+                            WHERE wp_posts.post_title LIKE '%$name%' ORDER BY wp_posts.post_modified DESC LIMIT $limit;");
+    $results = $wpdb->get_results( $sql );
+    // return $wpdb->print_error(); 
+    return $results;
+}
+
 add_action( 'rest_api_init', function () {
   register_rest_route( 'wp/v2', '/lastmodified', array(
     'methods' => 'GET',
