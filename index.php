517fbd8020c86ad0f12dae332e0736979aecc9f5
SaphireVert <jerome.cosandey@gmail.com>
2023-03-15 10:32:25 +0100

    feat: describe plugin

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: “WPLastPageChanges”
+Description: Gives the name of the last user who modified a given page.
+Version: 0.0.1
+Author: Jérôme Cosandey
+Author URI: https://github.com/saphirevert
+*/
