f8a911b8681d6bf0d7b39a667f255f9ae2cf865e
Lindo Duratti <durlindo@hotmail.com>
2025-03-20 08:14:42 +0100

    Update EPFL_custom_editor_menu_loader.php
    PEP8

--- EPFL_custom_editor_menu_loader.php
+++ EPFL_custom_editor_menu_loader.php
@@ -15,9 +15,8 @@ require (WPMU_PLUGIN_DIR.'/epfl-custom-editor-menu/epfl-custom-editor-menu.php')
 function remove_redirection_from_tools_menu_for_editor() {
     $user = wp_get_current_user();
     if (!in_array('administrator', $user->roles)) {
-	    remove_submenu_page('tools.php', 'redirection.php');
+        remove_submenu_page('tools.php', 'redirection.php');
     }
 }
 
 add_action('admin_menu', 'remove_redirection_from_tools_menu_for_editor', 999);
-
