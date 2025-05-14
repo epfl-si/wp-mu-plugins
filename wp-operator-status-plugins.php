b815a40de9ad2b772c0b528b6879a6e9906f0d5e
Rosa Maggi <rosa.maggi@epfl.ch>
2025-05-14 12:11:40 +0200

    [fix] Import and json format

--- wp-operator-status-plugins.php
+++ wp-operator-status-plugins.php
@@ -2,12 +2,14 @@
 
 namespace WP_Operator_Status_Plugins;
 
+use stdClass;
+
 add_filter("wp_operator_status", '\WP_Operator_Status_Plugins\report_plugins');
 
 function report_plugins ($status) {
   $status["plugins"] = [];
   foreach (get_option( 'active_plugins', array() ) as $plugin) {
-    $status["plugins"][$plugin] = new stdClass;
+    $status["plugins"][dirname($plugin)] = new stdClass;
   }
   return $status;
 }
