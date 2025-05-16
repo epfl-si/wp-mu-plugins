51ce87b14939780cb6359f90d605b6444755d126
Dominique Quatravaux <dominique@quatravaux.org>
2025-05-16 10:11:49 +0200

    [style] Single quotes throughout

--- wp-operator-status-plugins.php
+++ wp-operator-status-plugins.php
@@ -4,12 +4,12 @@ namespace WP_Operator_Status_Plugins;
 
 use stdClass;
 
-add_filter("wp_operator_status", '\WP_Operator_Status_Plugins\report_plugins');
+add_filter('wp_operator_status', '\WP_Operator_Status_Plugins\report_plugins');
 
 function report_plugins ($status) {
-  $status["plugins"] = [];
+  $status['plugins'] = [];
   foreach (get_option( 'active_plugins', array() ) as $plugin) {
-    $status["plugins"][dirname($plugin)] = new stdClass;
+    $status['plugins'][dirname($plugin)] = new stdClass;
   }
   return $status;
 }
