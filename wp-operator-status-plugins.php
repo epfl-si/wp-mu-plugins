629781fa8447e69855b5c1b348f8cee2d68acba1
Rosa Maggi <rosa.maggi@epfl.ch>
2025-06-19 15:21:34 +0200

    [feature] The `wp_operator_status` filter check now also languages list, unitId, title and tagline

--- wp-operator-status-plugins.php
+++ wp-operator-status-plugins.php
@@ -5,11 +5,42 @@ namespace WP_Operator_Status_Plugins;
 use stdClass;
 
 add_filter('wp_operator_status', '\WP_Operator_Status_Plugins\report_plugins');
+add_filter('wp_operator_status', '\WP_Operator_Status_Plugins\report_languages');
+add_filter('wp_operator_status', '\WP_Operator_Status_Plugins\report_tagline');
+add_filter('wp_operator_status', '\WP_Operator_Status_Plugins\report_title');
+add_filter('wp_operator_status', '\WP_Operator_Status_Plugins\report_unit');
 
 function report_plugins ($status) {
-  $status['plugins'] = [];
-  foreach (get_option( 'active_plugins', array() ) as $plugin) {
-    $status['plugins'][dirname($plugin)] = new stdClass;
-  }
-  return $status;
+	$status['plugins'] = [];
+	foreach (get_option( 'active_plugins', array() ) as $plugin) {
+		$status['plugins'][dirname($plugin)] = new stdClass;
+	}
+	return $status;
+}
+
+function report_languages ($status) {
+	$status['languages'] = [];
+	if (function_exists('pll_languages_list')) {
+		$languages = pll_languages_list( ['fields' => 'locale'] );
+
+		foreach ($languages as $lang) {
+			$status['languages'][] = $lang;
+		}
+	}
+	return $status;
+}
+
+function report_tagline ($status) {
+	$status['tagline'] = get_bloginfo('description');
+	return $status;
+}
+
+function report_title ($status) {
+	$status['title'] = get_bloginfo('title');
+	return $status;
+}
+
+function report_unit ($status) {
+	$status['unit'] = get_option('plugin:epfl_accred:unit_id');
+	return $status;
 }
