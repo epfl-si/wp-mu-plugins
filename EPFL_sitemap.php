6885a7675bed3f476cebe9e793a18ef84bceacd6
Saskya Panchaud <saskya.panchaud@epfl.ch>
2025-08-14 10:19:12 +0200

    [fix] Update file header for sitemap mu-plugin

--- EPFL_sitemap.php
+++ EPFL_sitemap.php
@@ -1,10 +1,36 @@
 <?php
-/*
-Plugin Name: EPFL sitemap
-Description: Give the sitemap for www.epfl.ch in xml format.
-Version: 1.0.0
-Author: ISAS-FSD
-*/
+/**
+ * EPFL Sitemap
+ *
+ * @package     EPFLSitemap
+ * @author      ISAS-FSD
+ * @copyright   Copyright (c) 2025, EPFL
+ * @license     GPL-2.0-or-later
+ *
+ * @wordpress-mu-plugins
+ * Must Use Plugin Name: EPFL Sitemap
+ * Plugin URI:  https://github.com/epfl-si/wp-mu-plugins
+ * Description: Give the sitemap for www.epfl.ch in xml format
+ * Version:     1.0.0
+ * Author:      ISAS-FSD
+ * Author URI:  https://go.epfl.ch/idev-fsd
+ * Text Domain: EPFL-Sitemap
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * EPFL Sitemap is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * EPFL Sitemap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EPFL Sitemap. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 function getSitemap() {
     $menu_api_host = "menu-api";
