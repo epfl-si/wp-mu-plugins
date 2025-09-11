d5e31306b95e5853cb2160df7edccf475df1d8f9
Rosa Maggi <rosa.maggi@epfl.ch>
2025-09-11 10:05:58 +0200

    [comment] Remove all legalese comment

--- EPFL_sitemap.php
+++ EPFL_sitemap.php
@@ -18,18 +18,6 @@
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
- * EPFL Sitemap is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * any later version.
- *
- * EPFL Sitemap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with EPFL Sitemap. If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace EPFL_sitemap;
