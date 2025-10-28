eb202c7b0c4435a8a7199c445e6e7b5e30fd6348
Rosa Maggi <rosa.maggi@epfl.ch>
2025-10-28 17:08:22 +0100

    [fix] Send with certificate

--- EPFL_audit.php
+++ EPFL_audit.php
@@ -106,9 +106,6 @@ function callOPDo($crudt, $description) {
 
 	$ch = curl_init($url);
 
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-
 	curl_setopt($ch, CURLOPT_POST, true);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //getenv("API_KEY")
