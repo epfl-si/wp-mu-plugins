2a755f0df29f874f761f6f62000a58d8f35d01f5
Rosa Maggi <rosa.maggi@epfl.ch>
2025-10-28 14:34:39 +0100

    [feature] Call OPDO API

--- EPFL_audit.php
+++ EPFL_audit.php
@@ -25,12 +25,11 @@ function save_page_function( $post_id, $post, $update ) {
 		return;
 	}
 
-	$user = wp_get_current_user();
 	if (in_array($post->post_type, ['post', 'page'])) {
-		error_log($post->post_type . ' "' . get_the_title($post_id) . '" (' . get_page_link($post_id) . ') was updated by ' . $user->user_email . ' (' . $user->user_nicename . ') for site ' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+		callOPDo('u',$post->post_type . ' "' . get_the_title($post_id) . '" (' . get_page_link($post_id) . ') was updated');
 	} elseif ($post->post_type == 'nav_menu_item') {
 		$menu_item = wp_setup_nav_menu_item( get_post( $post_id ) );
-		error_log($post->post_type . ' "' . $post_id . ' - ' . $menu_item->title . '" (' . $menu_item->url . ') was updated by ' . $user->user_email . ' (' . $user->user_nicename . ') for site ' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+		callOPDo('u', $post->post_type . ' "' . $post_id . ' - ' . $menu_item->title . '" (' . $menu_item->url . ') was updated');
 	}
 }
 
@@ -39,34 +38,93 @@ function delete_page_function( $post_id ) {
 	if ( wp_is_post_revision( $post_id ) ){
 		return;
 	}
-	$user = wp_get_current_user();
 	if (in_array($post->post_type, ['post', 'page'])) {
-		error_log($post->post_type . ' "' . get_the_title($post_id) . '" (' . get_page_link($post_id) . ') was deleted by ' . $user->user_email . ' (' . $user->user_nicename . ') for site ' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+		callOPDo('d', $post->post_type . ' "' . get_the_title($post_id) . '" (' . get_page_link($post_id) . ') was deleted');
 	} elseif ($post->post_type == 'nav_menu_item') {
-		error_log($post->post_type . ' ' . $post_id . ') was deleted by ' . $user->user_email . ' (' . $user->user_nicename . ') for site ' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+		callOPDo('d', $post->post_type . ' ' . $post_id . ') was deleted');
 	}
-
 }
 
-function create_nav_menu_function( $menu_id, $action ) {
-	$user = wp_get_current_user();
-	error_log('Menu ' . $menu_id . ') was ' . $action . ' by ' . $user->user_email . ' (' . $user->user_nicename . ') for site ' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+function nav_menu_function( $menu_id, $action, $action_label ) {
+	callOPDo($action, 'Menu ' . $menu_id . ' was ' . $action_label);
+}
+
+function user_function( $user_id, $action, $action_label ) {
+	callOPDo($action, 'User ' . $user_id . ' was ' . $action_label);
+}
+
+function option_function( $option, $value, $action, $old_value ) {
+	/*if ($action == 'c') callOPDo($action, "Option added $option = " . maybe_serialize( $value ) );
+	else if ($action == 'u') error_log( "Option modified : $option from " . maybe_serialize($old_value) . " to " . maybe_serialize($value) );
+	else if ($action == 'd') error_log( "Option deleted : $option" );*/
 }
 
 add_action( 'save_post', 'save_page_function', 10, 3 );
 add_action( 'delete_post', 'delete_page_function', 10 );
-
 add_action( 'wp_create_nav_menu', function( $menu_id ) {
-	$action = 'created';
-	create_nav_menu_function( $menu_id, $action );
+	nav_menu_function( $menu_id, 'c', 'created' );
 }, 10 );
 add_action( 'wp_update_nav_menu', function( $menu_id ) {
-	$action = 'updated';
-	create_nav_menu_function( $menu_id, $action );
+	nav_menu_function( $menu_id, 'u', 'updated' );
 }, 10 );
 add_action( 'wp_delete_nav_menu', function( $menu_id ) {
-	$action = 'deleted';
-	create_nav_menu_function( $menu_id, $action );
+	nav_menu_function( $menu_id, 'd', 'deleted' );
 }, 10 );
+add_action( 'user_register', function( $user_id ) {
+	user_function( $user_id, 'c', 'created' );
+}, 10, 1 );
+add_action( 'profile_update', function( $user_id ) {
+	user_function( $user_id, 'u', 'updated' );
+}, 10, 2 );
+add_action( 'delete_user', function( $user_id ) {
+	user_function( $user_id, 'd', 'deleted' );
+}, 10, 3 );
+add_action( 'add_option', function( $option, $value ) {
+	option_function( $option, $value, 'c', NULL );
+}, 10, 2 );
+add_action( 'update_option', function( $option, $old_value, $value ) {
+	option_function( $option, $value, 'u', $old_value );
+}, 10, 3 );
+add_action( 'deleted_option', function( $option ) {
+	option_function( $option, NULL, 'd', NULL );
+} );
 
-// TODO users, wpform, redirections, options
+// TODO wpform, redirections
+
+function callOPDo($crudt, $description) {
+	$user = wp_get_current_user();
+	$url = getenv('OPDO_URL');
+
+	$data = [
+		"@timestamp" => (new DateTime())->format(DateTime::ATOM),
+		"crudt" => $crudt,
+		"handled_id" => $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'],
+		"handler_id" => $user->user_email,
+		"payload" => 'Site: ' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . '\n' . $description,
+		"source" => 'wordpress'
+	];
+
+	$ch = curl_init($url);
+
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //getenv("API_KEY")
+	curl_setopt($ch, CURLOPT_HTTPHEADER, [
+		"Authorization: ApiKey " . getenv('OPDO_API_KEY'),
+		"Content-Type: application/json",
+		"Accept: application/json"
+	]);
+
+	$response = curl_exec($ch);
+
+	if (curl_errno($ch)) {
+		error_log('ERROR: ' . curl_error($ch));
+	} else {
+		error_log("Response: " . $response);
+	}
+
+	curl_close($ch);
+}
